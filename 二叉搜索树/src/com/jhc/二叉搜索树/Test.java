6bf1b7ec7ccdcf3fe54dc772b8ea3c5f4e32e2ca
jhc <13514409760@163.com>
2018-12-04 10:04:28 +0800

    平衡二叉树

---
+++ 二叉搜索树/src/com/jhc/二叉搜索树/Test.java
@@ -0,0 +1,34 @@
+package com.jhc.¶þ²æËÑË÷Ê÷;
+
+public class Test {
+	public static void main(String[] args){
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		
+		bst.insert(5);
+		bst.insert(7);
+		bst.insert(3);
+		bst.insert(1);
+		bst.insert(9);
+		bst.insert(6);
+		bst.insert(4);
+		System.out.println("Max "+bst.findMax());
+		System.out.println("Min "+bst.findMin());
+		System.out.println("9? "+bst.contains(9));
+		System.out.println("8? "+bst.contains(8));
+	
+		bst.LDRTree();
+		bst.remove(6);
+		System.out.println();
+		System.out.println("---------------------ÖÐÐò");
+		bst.LDRTree();
+		System.out.println();
+		System.out.println("Ç°Ðò");
+		bst.PreLDRTree();
+		System.out.println();
+		System.out.println("ºóÐò");
+		bst.AfterLDRTree();
+		System.out.println();
+		
+	}
+	
+}
